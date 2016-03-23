980bfe5076740395b2967eb8f36e15ff6b71dd3a
vsukhin <vsukhin@virtuososoft.com>
2016-03-23 12:12:04 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+date_default_timezone_set('UTC');
+?>
+<html>
+<body style="font-size: 14pt;">
+Hello, <?php echo $_SERVER['REMOTE_ADDR']; ?>
+<br>
+It is now <?php echo date(DATE_RFC2822); ?>
+<br>
+Served to you by
+<a href="http://nginx.org/">Nginx</a>,
+running on a
+<a href="http://rumpkernel.org">rump kernel</a>...
+<br>
+<?php
+   require "predis/autoload.php";
+   PredisAutoloader::register();
+
+   try {
+      // This connection is for a remote server
+      $redis = new PredisClient(array(
+         "scheme" => "tcp",
+         "host" => "redis",
+         "port" => 6379
+      ));
+
+      echo "Successfully connected to Redis";
+   }
+   catch (Exception $e) {
+      echo "Couldn't connected to Redis";  
+      die($e->getMessage());
+   }
+      
+   // sets message to contain "Hello world"
+   $redis->set('message', 'Hello world from Redis store');
+
+   // gets the value of message
+   $value = $redis->get('message');
+
+   // Hello world
+   print($value); 
+?>
+</body>
+</html>
